ddb1eab7548bb2380f53cab6a4bb8e71e99b74d0
Zheng Teck <adamgoh1998@gmail.com>
2024-02-28 19:37:44 +0800

    refactor: import statement

--- public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/parser/MkdirParserTest.java
+++ public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/parser/MkdirParserTest.java
@@ -7,7 +7,10 @@ import sg.edu.nus.comp.cs4218.impl.parser.MkdirParser;
 
 import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MkdirParserTest {
 
