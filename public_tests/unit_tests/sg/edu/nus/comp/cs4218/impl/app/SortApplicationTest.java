7e8d93b872cf533611d3ff82c2ae562f57e49e55
senwei01 <leesenwei01@gmail.com>
2024-02-28 13:50:31 +0800

    Add additional unit test for SortApplication

--- public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/app/SortApplicationTest.java
+++ public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/app/SortApplicationTest.java
@@ -1,4 +1,4 @@
-package public_tests.unit_tests.sg.edu.nus.comp.cs4218.impl.app;
+package unit_tests.sg.edu.nus.comp.cs4218.impl.app;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -7,10 +7,7 @@ import org.junit.jupiter.api.Test;
 import sg.edu.nus.comp.cs4218.app.SortInterface;
 import sg.edu.nus.comp.cs4218.exception.SortException;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -26,7 +23,6 @@ class SortApplicationTest {
 
     private static final String ROOT_DIRECTORY = sg.edu.nus.comp.cs4218.Environment.currentDirectory;
     private static final String TEST_DIRECTORY = ROOT_DIRECTORY + File.separator + "test_folder";
-    private static final String TEST_FOLDER_NAME = "tmpSortTestFolder" + File.separator;
     static String file = "file.txt";
     static String file2 = "file2.txt";
     static String path = TEST_DIRECTORY + file;
@@ -42,8 +38,8 @@ class SortApplicationTest {
 
         Files.createFile(Paths.get(path));
         Files.createFile(Paths.get(path2));
-        Files.write(Paths.get(path), ("10" + STRING_NEWLINE + "1" + STRING_NEWLINE + "2").getBytes(), APPEND);
-        Files.write(Paths.get(path2), ("a" + STRING_NEWLINE + "A" + STRING_NEWLINE + "ab" + STRING_NEWLINE + "AB").getBytes(), APPEND);
+        Files.write(Paths.get(path), ("10" + STRING_NEWLINE + "2" + STRING_NEWLINE + "1").getBytes(), APPEND);
+        Files.write(Paths.get(path2), ("a" + STRING_NEWLINE + "A" + STRING_NEWLINE + "ac" + STRING_NEWLINE + "AC").getBytes(), APPEND);
     }
 
     @BeforeEach
@@ -75,16 +71,50 @@ class SortApplicationTest {
 
     }
 
-    @org.junit.jupiter.api.Test
-    void run() {
+    @Test
+    void testRunWithInvalidArgumentsShouldThrow() {
+        String inputString = "ac" + STRING_NEWLINE + "a" + STRING_NEWLINE + "A" + STRING_NEWLINE + "AC";
+        InputStream input = new ByteArrayInputStream(inputString.getBytes());
+        String[] args = new String[]{"-z"};
+        assertThrows(SortException.class, () -> sortApplication.run(args, input, System.out));
     }
 
-    @org.junit.jupiter.api.Test
-    void sortFromFiles() {
+    @Test
+    void testRunWithEmptyArgumentsShouldPass() {
+        String inputData = "ac" + STRING_NEWLINE + "a" + STRING_NEWLINE + "A" + STRING_NEWLINE + "AC";
+        InputStream inputStream = new ByteArrayInputStream(inputData.getBytes());
+        String[] arguments = new String[]{};
+        assertDoesNotThrow(() -> sortApplication.run(arguments, inputStream, System.out));
     }
 
-    @org.junit.jupiter.api.Test
-    void sortFromStdin() {
+
+    @Test
+    void testSortFromStdinWithNumericFirstWordAndNoReverseOrderAndNotCaseIndependent() throws Exception {
+        String inputText = "10" + STRING_NEWLINE + "15" + STRING_NEWLINE + "13";
+        InputStream inputStream = new ByteArrayInputStream(inputText.getBytes());
+        String actualOutput = sortApplication.sortFromStdin(true, false, false, inputStream);
+        String expectedOutput = "10" + STRING_NEWLINE + "13" + STRING_NEWLINE + "15";
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void sortFromStdin_firstWordNumberReverseOrderCaseIndependent_returnsLines() throws Exception {
+        String inputText = "10" + STRING_NEWLINE + "15" + STRING_NEWLINE + "13";
+        InputStream inputStream = new ByteArrayInputStream(inputText.getBytes());
+        String actualOutput = sortApplication.sortFromStdin(true, true, true, inputStream);
+        String expectedOutput = "15" + STRING_NEWLINE + "13" + STRING_NEWLINE + "10";
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void sortFromStdin_letters() {
+        String inputText = "A" + STRING_NEWLINE + "b" + STRING_NEWLINE + "c" + STRING_NEWLINE + "D";
+        InputStream inputStream = new ByteArrayInputStream(inputText.getBytes());
+        String expectedOutput = "A" + STRING_NEWLINE + "D" + STRING_NEWLINE + "b" + STRING_NEWLINE + "c";
+        assertDoesNotThrow(() -> {
+            String actualOutput = sortApplication.sortFromStdin(false, false, false, inputStream);
+            assertEquals(expectedOutput, actualOutput);
+        });
     }
 
     private static void emptyFolder(File folder, boolean deleteFolder) {
