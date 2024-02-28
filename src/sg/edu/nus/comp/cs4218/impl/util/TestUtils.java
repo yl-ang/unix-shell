8666b237aeab80a2c0189e9a463d8bdf2c40a93f
Yu Qi Tan <83687861+yuqitanyq@users.noreply.github.com>
2024-02-28 16:39:54 +0800

    Add Unit tests for Ls and add testutils file for helper functions

---
+++ src/sg/edu/nus/comp/cs4218/impl/util/TestUtils.java
@@ -0,0 +1,19 @@
+package sg.edu.nus.comp.cs4218.impl.util;
+
+import java.io.File;
+
+public class TestUtils {
+    private TestUtils() {
+    }
+
+    public static void deleteDir(File file) {
+        File[] contents = file.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                deleteDir(f);
+            }
+        }
+        file.delete();
+    }
+
+}
