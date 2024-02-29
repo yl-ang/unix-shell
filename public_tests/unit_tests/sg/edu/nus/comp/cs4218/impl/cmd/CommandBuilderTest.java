f8210d14d989b36da56c1bad5f96b4df37fb869e
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-01 02:37:31 +0800

    Update CommandBuilderTest.java

--- public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/cmd/CommandBuilderTest.java
+++ public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/cmd/CommandBuilderTest.java
@@ -1,33 +1,49 @@
 package unit_tests.sg.edu.nus.comp.cs4218.impl.cmd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import sg.edu.nus.comp.cs4218.impl.util.ApplicationRunner;
 import sg.edu.nus.comp.cs4218.Command;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.cmd.CallCommand;
 import sg.edu.nus.comp.cs4218.impl.cmd.PipeCommand;
 import sg.edu.nus.comp.cs4218.impl.cmd.SequenceCommand;
+import sg.edu.nus.comp.cs4218.impl.util.ArgumentResolver;
 import sg.edu.nus.comp.cs4218.impl.util.CommandBuilder;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class CommandBuilderTest {
 
     @Mock
     ApplicationRunner applicationRunner;
 
+    @Mock
+    ArgumentResolver argumentResolver;
+
+    @InjectMocks
+    CommandBuilder commandBuilder;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     void parseCommand_NullCommandString_ShouldThrowShellException() {
         // WHEN / THEN
-        assertThrows(ShellException.class, () -> CommandBuilder.parseCommand(null, applicationRunner));
+        assertThrows(ShellException.class, () -> commandBuilder.parseCommand(null, applicationRunner));
     }
 
     @Test
     void parseCommand_BlankCommandString_ShouldThrowShellException() {
         // WHEN / THEN
-        assertThrows(ShellException.class, () -> CommandBuilder.parseCommand("", applicationRunner));
+        assertThrows(ShellException.class, () -> commandBuilder.parseCommand("", applicationRunner));
     }
 }
