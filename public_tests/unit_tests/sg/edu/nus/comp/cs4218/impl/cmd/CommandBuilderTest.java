924c4047c207d391f4fd465a404e5e0b6a44cf0e
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-01 01:06:12 +0800

    Create CommandBuilderTest.java

---
+++ public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/cmd/CommandBuilderTest.java
@@ -0,0 +1,27 @@
+package unit_tests.sg.edu.nus.comp.cs4218.impl.cmd;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import sg.edu.nus.comp.cs4218.impl.util.ApplicationRunner;
+import sg.edu.nus.comp.cs4218.Command;
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+import sg.edu.nus.comp.cs4218.impl.cmd.CallCommand;
+import sg.edu.nus.comp.cs4218.impl.cmd.PipeCommand;
+import sg.edu.nus.comp.cs4218.impl.cmd.SequenceCommand;
+import sg.edu.nus.comp.cs4218.impl.util.CommandBuilder;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CommandBuilderTest {
+
+    @Mock
+    ApplicationRunner applicationRunner;
+
+    @Test
+    void parseCommand_NullCommandString_ShouldThrowShellException() {
+        // WHEN / THEN
+        assertThrows(ShellException.class, () -> CommandBuilder.parseCommand(null, applicationRunner));
+    }
+}
