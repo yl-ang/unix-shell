aac1d75993963d91d7275e67aa225d3c3c32e53f
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-02-29 19:04:49 +0800

    Update exit application for test

--- public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/app/ExitApplicationTest.java
+++ public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/app/ExitApplicationTest.java
@@ -1,40 +1,23 @@
 package unit_tests.sg.edu.nus.comp.cs4218.impl.app;
 
 import org.junit.jupiter.api.Test;
-import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.impl.app.ExitApplication;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import static org.mockito.Mockito.*;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-public class ExitApplicationTest {
-    private static final ExitApplication exitApplication = new ExitApplication();
+class ExitApplicationTest {
+    ExitApplication exitApplication = new ExitApplication();
 
     @Test
-    void run_ExitApplication_ShouldTerminateExecution() {
-        assertThrows(AbstractApplicationException.class, () -> {
-            exitApplication.run(null, null, null);
-        });
-    }
+    void testTerminateExecutionWithCustomExitAction() throws Exception {
+        // Given
+        Runnable mockExitAction = mock(Runnable.class);
+        exitApplication.setExitAction(mockExitAction);
 
-    @Test
-    void run_terminateExecution_ExitApplication_ShouldTerminateExecution() {
-        assertThrows(AbstractApplicationException.class, () -> {
-            exitApplication.terminateExecution();
-        });
-    }
+        // When
+        exitApplication.terminateExecution();
 
-    @Test
-    void run_ExitApplicationWithStreams_ShouldTerminateExecution() {
-        InputStream inputStream = new ByteArrayInputStream("".getBytes());
-        OutputStream outputStream = new ByteArrayOutputStream();
-
-        assertThrows(AbstractApplicationException.class, () -> {
-            exitApplication.run(null, inputStream, outputStream);
-        });
+        // Then
+        verify(mockExitAction, times(1)).run();
     }
 }
