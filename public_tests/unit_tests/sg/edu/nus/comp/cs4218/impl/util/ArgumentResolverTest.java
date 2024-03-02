6af9d7fd23fae0556f1a28ef5fa4d9860f6039f9
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-03 05:06:33 +0800

    Add parseArguments_ShouldParseMultipleArguments
    Using Spy mockito

--- public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/util/ArgumentResolverTest.java
+++ public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/util/ArgumentResolverTest.java
@@ -14,31 +14,56 @@ import sg.edu.nus.comp.cs4218.impl.util.RegexArgument;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.*;
 
 public class ArgumentResolverTest {
-
-    @Mock
-    private ApplicationRunner applicationRunnerMock;
-
-    @Mock
-    private RegexArgument regexArgumentMock;
 
     @Mock
     private Command commandMock;
 
     @InjectMocks
-    private ArgumentResolver argumentResolver;
+    @Spy
+    private ArgumentResolver argumentResolverSpy;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void parseArguments_ShouldParseMultipleArguments() throws AbstractApplicationException, ShellException, FileNotFoundException {
+
+        // GIVEN
+        List<String> inputArgsList = Arrays.asList("arg1", "'arg 2'", "`ls`");
+
+        // Mocking resolveOneArgument method to isolate the test
+        doReturn(List.of("arg1")).when(argumentResolverSpy).resolveOneArgument("arg1");
+        doReturn(List.of("arg 2")).when(argumentResolverSpy).resolveOneArgument("'arg 2'");
+        doReturn(Arrays.asList("file1", "file2")).when(argumentResolverSpy).resolveOneArgument("`ls`");
+
+        // WHEN
+        List<String> result = argumentResolverSpy.parseArguments(inputArgsList);
+
+        // THEN
+        assertEquals(Arrays.asList("arg1", "arg 2", "file1", "file2"), result);
+    }
+
+    @Test
+    void parseArguments_ShouldHandleEmptyArgumentList() throws AbstractApplicationException, ShellException, FileNotFoundException {
+        // GIVEN
+        List<String> inputArgsList = List.of();
+
+        // WHEN
+        List<String> result = argumentResolverSpy.parseArguments(inputArgsList);
+
+        // THEN
+        assertEquals(List.of(), result);
     }
 
     @Test
@@ -60,7 +85,7 @@ public class ArgumentResolverTest {
             String inputArg = "`ls`";
 
             // WHEN
-            List<String> result = argumentResolver.resolveOneArgument(inputArg);
+            List<String> result = argumentResolverSpy.resolveOneArgument(inputArg);
 
             // THEN
             assertEquals(List.of("test.txt"), result);
@@ -68,5 +93,41 @@ public class ArgumentResolverTest {
             // Verify interactions on the mocked static method
             mockedCommandBuilder.verify(() -> CommandBuilder.parseCommand(anyString(), any(ApplicationRunner.class)));
         }
+    }
+
+    @Test
+    void resolveOneArgument_ShouldHandleAsterisk() throws AbstractApplicationException, ShellException, FileNotFoundException {
+        // GIVEN
+        String inputArg = "file*.txt";
+
+        // WHEN
+        List<String> result = argumentResolverSpy.resolveOneArgument(inputArg);
+
+        // THEN
+        assertEquals(List.of("file*.txt"), result);
+    }
+
+    @Test
+    void resolveOneArgument_ShouldHandleQuoting() throws AbstractApplicationException, ShellException, FileNotFoundException {
+        // GIVEN
+        String inputArg = "\"double-quoted text\"";
+
+        // WHEN
+        List<String> result = argumentResolverSpy.resolveOneArgument(inputArg);
+
+        // THEN
+        assertEquals(List.of("double-quoted text"), result);
+    }
+
+    @Test
+    void resolveOneArgument_ShouldHandleSingleQuote() throws AbstractApplicationException, ShellException, FileNotFoundException {
+        // GIVEN
+        String inputArg = "'single-quoted text'";
+
+        // WHEN
+        List<String> result = argumentResolverSpy.resolveOneArgument(inputArg);
+
+        // THEN
+        assertEquals(List.of("single-quoted text"), result);
     }
 }
