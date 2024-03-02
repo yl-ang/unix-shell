5e15042cb21911f1b470fa6f360bbb933956c9c2
yongler <leeyongler1.618@gmail.com>
2024-03-02 21:03:48 +0800

    Resolve comments

--- src/sg/edu/nus/comp/cs4218/impl/parser/WcArgsParser.java
+++ src/sg/edu/nus/comp/cs4218/impl/parser/WcArgsParser.java
@@ -3,30 +3,30 @@ package sg.edu.nus.comp.cs4218.impl.parser;
 import java.util.List;
 
 public class WcArgsParser extends ArgsParser {
-    public static final char BYTE_COUNT_FLAG = 'c';
+    public static final char FLAG_IS_BYTE_COUNT = 'c';
 
-    public static final char LINE_COUNT_FLAG = 'l';
+    public static final char FLAG_IS_LINE_COUNT = 'l';
 
-    public static final char WORD_COUNT_FLAG = 'w';
+    public static final char FLAG_IS_WORD_COUNT = 'w';
 
     public WcArgsParser() {
         super();
 
-        legalFlags.add(BYTE_COUNT_FLAG);
-        legalFlags.add(LINE_COUNT_FLAG);
-        legalFlags.add(WORD_COUNT_FLAG);
+        legalFlags.add(FLAG_IS_BYTE_COUNT);
+        legalFlags.add(FLAG_IS_LINE_COUNT);
+        legalFlags.add(FLAG_IS_WORD_COUNT);
     }
 
     public Boolean isByteCount() {
-        return flags.contains(BYTE_COUNT_FLAG);
+        return flags.contains(FLAG_IS_BYTE_COUNT);
     }
 
     public Boolean isLineCount() {
-        return flags.contains(LINE_COUNT_FLAG);
+        return flags.contains(FLAG_IS_LINE_COUNT);
     }
 
     public Boolean isWordCount() {
-        return flags.contains(WORD_COUNT_FLAG);
+        return flags.contains(FLAG_IS_WORD_COUNT);
     }
 
     public List<String> getFileNames() {
