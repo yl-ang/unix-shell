18a9278d282c6665de6883cc878e738426d10554
senwei01 <leesenwei01@gmail.com>
2024-03-03 15:38:03 +0800

    Added more unit test cases

--- public_tests/tdd/unit_tests/sg/edu/nus/comp/cs4218/impl/UniqApplicationTest.java
+++ public_tests/tdd/unit_tests/sg/edu/nus/comp/cs4218/impl/UniqApplicationTest.java
@@ -5,13 +5,11 @@ import sg.edu.nus.comp.cs4218.app.UniqInterface;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -21,12 +19,11 @@ public class UniqApplicationTest {
 
     private UniqInterface uniqApplication;
     private static InputStream inputStdin;
-    private InputStream inputTestFile;
-    private InputStream inputEmptyTestFile;
-    private OutputStream output;
+    private OutputStream outputStdout;
+
 
     private static final String TEST_FILENAME = "uniqTestFile.txt";
-    private static final String EMPTY_TEST_FILENAME = "uniqEmptyTestFile.txt";
+    private static final String OUTPUTTEST_FILENAME = "uniqTestFile2.txt";
     private static final String TEST_INPUT = "HELLO_WORLD" + STRING_NEWLINE +
             "HELLO_WORLD" + STRING_NEWLINE +
             "ALICE" + STRING_NEWLINE +
@@ -51,26 +48,25 @@ public class UniqApplicationTest {
 
     @BeforeAll
     static void setUp() throws IOException {
-        inputStdin = System.in;
+        Path path1 = Paths.get(TEST_FILENAME);
+        Path path2 = Paths.get(OUTPUTTEST_FILENAME);
 
-        Path path = Paths.get(TEST_FILENAME);
-        Files.writeString(path, TEST_INPUT);
+        Files.deleteIfExists(path1);
+        Files.deleteIfExists(path2);
+        Files.writeString(path1, TEST_INPUT);
     }
 
     @BeforeEach
     void init() throws ShellException {
         uniqApplication = mock(UniqInterface.class);
-
-        inputTestFile = IOUtils.openInputStream(TEST_FILENAME);
-        inputEmptyTestFile = IOUtils.openInputStream(EMPTY_TEST_FILENAME);
-
-        output = new ByteArrayOutputStream();
+        inputStdin = new ByteArrayInputStream(new byte[]{});
+        outputStdout = new ByteArrayOutputStream();
     }
 
     @AfterEach
     void done() throws ShellException {
-        IOUtils.closeInputStream(inputTestFile);
-        IOUtils.closeInputStream(inputEmptyTestFile);
+        IOUtils.closeInputStream(inputStdin);
+        IOUtils.closeOutputStream(outputStdout);
     }
 
     @AfterAll
@@ -78,15 +74,107 @@ public class UniqApplicationTest {
         Path path = Paths.get(TEST_FILENAME);
         Files.deleteIfExists(path);
 
-        path = Paths.get(EMPTY_TEST_FILENAME);
+        path = Paths.get(OUTPUTTEST_FILENAME);
         Files.deleteIfExists(path);
     }
 
+    // Not sure if it will read the content correctly, after implementation will relook into this
     @Test
-    public void uniqFromFile_OptionC_returnsExpectedOutput() throws Exception {
-        String output = uniqApplication.uniqFromFile(true, false, false, TEST_INPUT, null);
+    public void uniqFromFile_NoOption_returnsExpectedOutputToFile() throws Exception {
+        String output = uniqApplication.uniqFromFile(false, false, false, TEST_FILENAME, OUTPUTTEST_FILENAME);
+        Path path = Paths.get(OUTPUTTEST_FILENAME);
+        Files.write(path, output.getBytes());
+
+        // Read the content of the output test file
+        String actualOutput = new String(Files.readAllBytes(path));
+        assertEquals(EXPECTEDOUTPUT_UNIQ, actualOutput);
+    }
+
+    @Test
+    public void uniqFromFile_AllTrue_returnsExpectedOutputToFile() throws Exception {
+        String expected = "2 " + "HELLO_WORLD" + STRING_NEWLINE +
+                "2 " + "HELLO_WORLD" + STRING_NEWLINE +
+                "2 " + "ALICE" + STRING_NEWLINE +
+                "2 " + "ALICE";
+        String output = uniqApplication.uniqFromFile(true, true, true, TEST_FILENAME, OUTPUTTEST_FILENAME);
+        Path path = Paths.get(OUTPUTTEST_FILENAME);
+        Files.write(path, output.getBytes());
+
+        // Read the content of the output test file
+        String content = new String(Files.readAllBytes(path));
+        assertEquals(expected, content);
+    }
+
+    @Test
+    public void uniqFromFile_OptionC_returnsExpectedOutputToFile() throws Exception {
+        String output = uniqApplication.uniqFromFile(true, false, false, TEST_FILENAME, OUTPUTTEST_FILENAME);
+        Path path = Paths.get(OUTPUTTEST_FILENAME);
+        Files.write(path, output.getBytes());
+
+        // Read the content of the output test file
+        String actualOutput = new String(Files.readAllBytes(path));
+        assertEquals(EXPECTEDOUTPUT_C, actualOutput);
+    }
+
+    @Test
+    public void uniqFromFile_OptionSmallD_returnsExpectedOutputToFile() throws Exception {
+        String output = uniqApplication.uniqFromFile(false, true, false, TEST_FILENAME, OUTPUTTEST_FILENAME);
+        Path path = Paths.get(OUTPUTTEST_FILENAME);
+        Files.write(path, output.getBytes());
+
+        // Read the content of the output test file
+        String actualOutput = new String(Files.readAllBytes(path));
+        assertEquals(EXPECTEDOUTPUT_SMALLD, actualOutput);
+    }
+
+    @Test
+    public void uniqFromFile_OptionD_returnsExpectedOutputToFile() throws Exception {
+        String output = uniqApplication.uniqFromFile(false, false, true, TEST_FILENAME, OUTPUTTEST_FILENAME);
+        Path path = Paths.get(OUTPUTTEST_FILENAME);
+        Files.write(path, output.getBytes());
+
+        // Read the content of the output test file
+        String actualOutput = new String(Files.readAllBytes(path));
+        assertEquals(EXPECTEDOUTPUT_D, actualOutput);
+    }
+
+    @Test
+    void uniqFromStdin_NoOption_returnsExpectedOutput() throws Exception {
+        inputStdin = new ByteArrayInputStream(TEST_INPUT.getBytes());
+        String output = uniqApplication.uniqFromStdin(false, false, false, inputStdin, null);
+        assertEquals(EXPECTEDOUTPUT_UNIQ, output);
+    }
+
+    @Test
+    void uniqFromStdin_OptionC_returnsExpectedOutput() throws Exception {
+        inputStdin = new ByteArrayInputStream(TEST_INPUT.getBytes());
+        String output = uniqApplication.uniqFromStdin(true, false, false, inputStdin, null);
         assertEquals(EXPECTEDOUTPUT_C, output);
     }
 
+    @Test
+    void uniqFromStdin_OptionSmallD_returnsExpectedOutput() throws Exception {
+        inputStdin = new ByteArrayInputStream(TEST_INPUT.getBytes());
+        String output = uniqApplication.uniqFromStdin(false, true, false, inputStdin, null);
+        assertEquals(EXPECTEDOUTPUT_SMALLD, output);
+    }
+
+    @Test
+    void uniqFromStdin_OptionD_returnsExpectedOutput() throws Exception {
+        inputStdin = new ByteArrayInputStream(TEST_INPUT.getBytes());
+        String output = uniqApplication.uniqFromStdin(false, false, true, inputStdin, null);
+        assertEquals(EXPECTEDOUTPUT_D, output);
+    }
+
+    @Test
+    public void uniqFromStdin_AllTrue_returnsExpectedOutput() throws Exception {
+        inputStdin = new ByteArrayInputStream(TEST_INPUT.getBytes());
+        String expected = "2 " + "HELLO_WORLD" + STRING_NEWLINE +
+                "2 " + "HELLO_WORLD" + STRING_NEWLINE +
+                "2 " + "ALICE" + STRING_NEWLINE +
+                "2 " + "ALICE";
+        String output = uniqApplication.uniqFromStdin(true, true, true, inputStdin, null);
+        assertEquals(expected, output);
+    }
 
 }
