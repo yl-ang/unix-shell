69c6d1dc10150bed04932557f3a0bec7720bd318
yongler <leeyongler1.618@gmail.com>
2024-03-12 17:04:02 +0800

    feat: add cutParser

--- src/sg/edu/nus/comp/cs4218/impl/parser/CutArgsParser.java
+++ src/sg/edu/nus/comp/cs4218/impl/parser/CutArgsParser.java
@@ -1,17 +1,47 @@
 package sg.edu.nus.comp.cs4218.impl.parser;
 
+import sg.edu.nus.comp.cs4218.exception.InvalidArgsException;
+
 import java.util.List;
 
 public class CutArgsParser extends ArgsParser {
     public static final char FLAG_IS_BYTE_CUT = 'b';
 
     public static final char FLAG_IS_CHAR_CUT = 'c';
+    private List<int[]> ranges;
 
     public CutArgsParser() {
         super();
 
         legalFlags.add(FLAG_IS_BYTE_CUT);
         legalFlags.add(FLAG_IS_CHAR_CUT);
+    }
+
+    @Override
+    public void parse(String... args) throws InvalidArgsException {
+        super.parse(args);
+
+        for (String arg : nonFlagArgs) {
+            if (arg.contains("-") || arg.matches("\\d+")) {
+                ranges.add(parseRanges(arg));
+            }
+        }
+    }
+
+    private int[] parseRanges(String arg) {
+        String[] splitRanges = arg.split(",");
+        for (String range : splitRanges) {
+            if (range.contains("-")) {
+                String[] bounds = range.split("-");
+                int start = Integer.parseInt(bounds[0]);
+                int end = Integer.parseInt(bounds[1]);
+                ranges.add(new int[]{start, end});
+            } else {
+                int num = Integer.parseInt(range);
+                ranges.add(new int[]{num, num});
+            }
+        }
+
     }
 
     public Boolean isByteCut() {
@@ -24,5 +54,9 @@ public class CutArgsParser extends ArgsParser {
 
     public List<String> getFileNames() {
         return nonFlagArgs;
+    }
+
+    public List<int[]> getRanges() {
+        return ranges;
     }
 }
