50bef047d826e4b69ec2963ac2f800ad9acdb2e9
Yu Qi Tan <83687861+yuqitanyq@users.noreply.github.com>
2024-03-14 17:36:33 +0800

    Implement MvApplication, MvUtils, new ErrorConstants

--- src/sg/edu/nus/comp/cs4218/impl/app/MvApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/MvApplication.java
@@ -1,41 +1,27 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import sg.edu.nus.comp.cs4218.app.MvInterface;
-import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.InvalidArgsException;
 import sg.edu.nus.comp.cs4218.exception.MvException;
 import sg.edu.nus.comp.cs4218.impl.parser.MvArgsParser;
 import sg.edu.nus.comp.cs4218.impl.util.MvUtils;
-import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
 
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
+import java.nio.file.StandardCopyOption;
 
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_FILE_NOT_FOUND;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_INVALID_FLAG;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_ARGS;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_FILE_ARGS;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_OSTREAM;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NULL_ARGS;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_IS_NOT_DIR;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_TOO_MANY_ARGS;
 
 public class MvApplication implements MvInterface {
     @Override
     public void run(String[] args, InputStream stdin, OutputStream stdout) throws MvException {
-        if (args == null) {
-            throw new MvException(ERR_NULL_ARGS);
-        }
-        if (args.length < 2) {
-            throw new MvException(ERR_NO_ARGS);
-        }
-
-        if (stdout == null) {
-            throw new MvException(ERR_NO_OSTREAM);
-        }
+        MvUtils.checkArgsIsNull(args);
+        MvUtils.checkArgsHasFiles(args);
+        MvUtils.checkOutputIsNull(stdout);
 
         MvArgsParser parser = new MvArgsParser();
         try {
@@ -44,29 +30,67 @@ public class MvApplication implements MvInterface {
             throw new MvException(e.getMessage());
         }
         Boolean isOverwrite = parser.isOverwrite();
-
-        if (parser.getTargetFile() == null || parser.getSrcFiles() == null) {
-            throw new MvException(ERR_NO_ARGS);
-        }
+        MvUtils.checkIsSrcAndTargetFileNull(parser.getSrcFiles(), parser.getTargetFile());
 
         String[] srcFiles = parser.getSrcFiles()
                 .toArray(new String[0]);
         String targetFile = parser.getTargetFile();
 
-        if (srcFiles.length == 1) {
+        if (MvUtils.isDirectory(targetFile)) {
+            mvFilesToFolder(isOverwrite, targetFile, srcFiles);
+        } else if (srcFiles.length == 1) {
             mvSrcFileToDestFile(isOverwrite, srcFiles[0], targetFile);
         } else {
-            mvFilesToFolder(isOverwrite, targetFile, srcFiles);
+            throw new MvException(ERR_TOO_MANY_ARGS);
         }
     }
 
     @Override
     public String mvSrcFileToDestFile(Boolean isOverwrite, String srcFile, String destFile) throws MvException {
-        return null;
+        Path absoluteSrcPath = Path.of(MvUtils.getNormalizedPath(srcFile));
+        Path absoluteTargetPath = Path.of(MvUtils.getNormalizedPath(destFile));
+
+        if (MvUtils.isDirectory(srcFile) && !MvUtils.isDirectory(destFile)) {
+            throw new MvException(ERR_IS_NOT_DIR + ": " + destFile);
+        }
+
+        MvUtils.checkFileExists(srcFile);
+        MvUtils.checkFileExists(destFile);
+        MvUtils.checkSrcIsWritable(srcFile);
+        MvUtils.checkSrcAndTargetAreDifferent(absoluteSrcPath, absoluteTargetPath);
+        try {
+            if (isOverwrite) {
+                MvUtils.checkDestIsWritable(absoluteTargetPath, destFile);
+                Files.move(absoluteSrcPath, absoluteTargetPath, StandardCopyOption.REPLACE_EXISTING);
+            } else {
+                Files.move(absoluteSrcPath, absoluteTargetPath);
+            }
+        } catch (IOException e) {
+            throw new MvException(e.getMessage());
+        }
+        return destFile;
     }
 
     @Override
     public String mvFilesToFolder(Boolean isOverwrite, String destFolder, String... fileName) throws MvException {
-        return null;
+        MvUtils.checkFileExists(destFolder);
+
+        if (!MvUtils.isDirectory(destFolder)) {
+            throw new MvException(ERR_IS_NOT_DIR + ": " + destFolder);
+        }
+
+        MvException mvException = null;
+        for (String srcFile : fileName) {
+            try {
+                mvSrcFileToDestFile(isOverwrite, srcFile, destFolder);
+            } catch (MvException e) {
+                mvException = e;
+            }
+        }
+        if (mvException != null) {
+            throw mvException;
+        }
+
+        return destFolder;
     }
 }
--- src/sg/edu/nus/comp/cs4218/impl/util/MvUtils.java
+++ src/sg/edu/nus/comp/cs4218/impl/util/MvUtils.java
@@ -4,23 +4,69 @@ import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.MvException;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.CopyOption;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_BOTH_PATHS_SAME;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_DIR_NOT_EMPTY;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_FILE_NOT_FOUND;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_IS_DIR;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_IS_NOT_DIR;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NOT_WRITEABLE;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_OVERWRITE_FAILED;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_SRC_SUBDIRECTORY_DEST;
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_TOO_MANY_ARGS;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_FILE_ARGS;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_OSTREAM;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NULL_ARGS;
 
 public class MvUtils {
+    /**
+     * Check if arguments are null
+     *
+     * @param args arguments received
+     * @throws MvException
+     */
+    public static void checkArgsIsNull(String[] args) throws MvException{
+        if (args == null) {
+            throw new MvException(ERR_NULL_ARGS);
+        }
+    }
+
+    /**
+     * Check if arguments has files
+     *
+     * @param args arguments received
+     * @throws MvException
+     */
+    public static void checkArgsHasFiles(String[] args) throws MvException{
+        if (args.length < 2) {
+            throw new MvException(ERR_NO_FILE_ARGS);
+        }
+    }
+
+    /**
+     * Check if output is null
+     *
+     * @param output output received
+     * @throws MvException
+     */
+    public static void checkOutputIsNull(OutputStream output) throws MvException{
+        if (output == null) {
+            throw new MvException(ERR_NO_OSTREAM);
+        }
+    }
+
+    /**
+     * Check if src and target file is null
+     *
+     * @param src src files received
+     * @param target target files received
+     * @throws MvException
+     */
+    public static void checkIsSrcAndTargetFileNull(List<String> src, String target) throws MvException{
+        if (src == null || target == null) {
+            throw new MvException(ERR_NO_FILE_ARGS);
+        }
+    }
+
     /**
      * returns the normalized path.
      *
@@ -29,6 +75,79 @@ public class MvUtils {
     public static String getNormalizedPath(String path) {
         Path resolvedPath = Paths.get(Environment.currentDirectory).resolve(path).normalize();
         return resolvedPath.toString();
+    }
+
+    /**
+     * checks if path provided is a directory.
+     *
+     * @param path to check if directory
+     */
+    public static boolean isDirectory(String path) {
+        Path resolvedPath = Paths.get(Environment.currentDirectory).resolve(path).normalize();
+        return Files.isDirectory(resolvedPath);
+    }
+
+    /**
+     * Check if the provided file exists.
+     *
+     * @param srcFile of path to source file
+     * @throws MvException
+     */
+    public static void checkFileExists(String srcFile) throws MvException {
+        File checkFile = new File(getNormalizedPath(srcFile));
+        if (!checkFile.exists()) {
+            throw new MvException(ERR_FILE_NOT_FOUND + ": " + srcFile);
+        }
+    }
+
+    /**
+     * Check and ensures that the absolute source path and absolute target path are different.
+     *
+     * @param absoluteSrcPath absolute path of source
+     * @param absoluteTargetPath absolute path of target
+     * @throws MvException
+     */
+    public static void checkSrcAndTargetAreDifferent(Path absoluteSrcPath, Path absoluteTargetPath) throws MvException {
+        if (absoluteSrcPath.equals(absoluteTargetPath)) {
+            throw new MvException(ERR_BOTH_PATHS_SAME + ": " + absoluteTargetPath + "=" + absoluteSrcPath);
+        }
+    }
+
+    /**
+     * Check and ensures that the srcFile can be written.
+     *
+     * @param srcFile of path to source file
+     * @throws MvException
+     */
+    public static void checkSrcIsWritable(String srcFile) throws MvException {
+        if (!isWriteable(srcFile)) {
+            throw new MvException(ERR_NOT_WRITEABLE + "src:" + srcFile);
+        }
+    }
+
+    /**
+     * checks if path provided contains a file that can be written to.
+     *
+     * @param path to check if is writeable
+     */
+    private static boolean isWriteable(String path) {
+        Path resolvedPath = Paths.get(Environment.currentDirectory).resolve(path).normalize();
+        File file = resolvedPath.toFile();
+        return file.canWrite();
+    }
+
+    /**
+     * Check and ensures that the destination file can be written to.
+     *
+     * @param absoluteDestPath absolute path of destination
+     * @param destFile of path to destination file
+     * @throws MvException
+     */
+    public static void checkDestIsWritable(Path absoluteDestPath, String destFile) throws MvException {
+        File destinationFile = new File(String.valueOf(absoluteDestPath));
+        if (!isWriteable(destFile) && destinationFile.exists()) {
+            throw new MvException(ERR_NOT_WRITEABLE + "dest:" + destFile);
+        }
     }
 
 }
