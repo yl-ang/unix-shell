93568ed4eb3b2d1e9abaa445ba055fce088aa5e5
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-17 20:54:24 +0800

    Add rm to ApplicationRunner

--- src/sg/edu/nus/comp/cs4218/impl/util/ApplicationRunner.java
+++ src/sg/edu/nus/comp/cs4218/impl/util/ApplicationRunner.java
@@ -22,6 +22,7 @@ public class ApplicationRunner {
     public final static String APP_CAT = "cat";
     public final static String APP_MKDIR = "mkdir";
     public final static String APP_SORT = "sort";
+    public final static String APP_RM = "rm";
 
     /**
      * Run the application as specified by the application command keyword and arguments.
@@ -67,6 +68,9 @@ public class ApplicationRunner {
                 break;
             case APP_SORT:
                 application = new SortApplication();
+                break;
+            case APP_RM:
+                application = new RmApplication();
                 break;
             default:
                 throw new ShellException(app + ": " + ERR_INVALID_APP);
