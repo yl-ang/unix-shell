634964709cee259d84888a200712ef0670b4c8bc
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-17 20:53:54 +0800

    Update parser

--- src/sg/edu/nus/comp/cs4218/impl/parser/RmArgsParser.java
+++ src/sg/edu/nus/comp/cs4218/impl/parser/RmArgsParser.java
@@ -1,20 +1,26 @@
 package sg.edu.nus.comp.cs4218.impl.parser;
 
+import java.util.List;
+
 public class RmArgsParser extends ArgsParser {
-    public static final char FLAG_IS_RECURSIVE = 'r';
-    public static final char FLAG_IS_REMOVE_EMPTY_DIR = 'd';
+    private static final char FLAG_IS_RECURSIVE = 'r';
+    private static final char FLAG_IS_EMPTY_FOLDER = 'd';
 
     public RmArgsParser() {
         super();
         legalFlags.add(FLAG_IS_RECURSIVE);
-        legalFlags.add(FLAG_IS_REMOVE_EMPTY_DIR);
+        legalFlags.add(FLAG_IS_EMPTY_FOLDER);
     }
 
     public Boolean isRecursive() {
         return flags.contains(FLAG_IS_RECURSIVE);
     }
 
-    public Boolean isRemoveEmptyDirectory() {
-        return flags.contains(FLAG_IS_REMOVE_EMPTY_DIR);
+    public Boolean isEmptyFolder() {
+        return flags.contains(FLAG_IS_EMPTY_FOLDER);
+    }
+
+    public List<String> getFileNames() {
+        return nonFlagArgs;
     }
 }
