65fc4b8bf898ac031df53d7ba1dde1b4b1cf0641
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-21 10:13:14 +0800

    test: add more tests to up coverage

--- public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/app/RmApplicationTest.java
+++ public_tests/unit_tests/sg/edu/nus/comp/cs4218/impl/app/RmApplicationTest.java
@@ -8,10 +8,10 @@ import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.RmException;
 import sg.edu.nus.comp.cs4218.impl.app.RmApplication;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RmApplicationTest {
@@ -23,21 +23,27 @@ public class RmApplicationTest {
 
     private File file;
     private File directory;
+    private File emptyDirectory;
 
     @BeforeEach
     void setUp() throws IOException {
         rmApplication = new RmApplication();
+
         file = new File(tempDir.toFile(), "file.txt");
         file.createNewFile();
+
         directory = new File(tempDir.toFile(), "directory");
         directory.mkdir();
+
+        emptyDirectory = new File(tempDir.toFile(), "emptyDirectory");
+        emptyDirectory.mkdir();
     }
 
     @AfterEach
     void tearDown() {
-        // Clean up files and directories created during tests
         file.delete();
         directory.delete();
+        emptyDirectory.delete();
     }
 
     @Test
@@ -71,5 +77,56 @@ public class RmApplicationTest {
     @Test
     void remove_DeleteDirectoryWithRecursive_Success() throws AbstractApplicationException {
         rmApplication.remove(false, true, directory.getPath());
+    }
+
+    @Test
+    void remove_EmptyDirectoryWithEmptyOption_Success() throws AbstractApplicationException {
+        // GIVEN
+        rmApplication.remove(true, false, emptyDirectory.getPath());
+
+        // WHEN
+        assert !emptyDirectory.exists();
+    }
+
+    @Test
+    void run_CallsRemove_Success() throws AbstractApplicationException {
+
+        // GIVEN
+        String[] args = {file.getAbsolutePath()};
+        InputStream inputStream = System.in;
+        OutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            // WHEN
+            rmApplication.run(args, inputStream, outputStream);
+        } catch (AbstractApplicationException e) {
+            e.printStackTrace();
+        }
+
+        // THEN
+        assert !file.exists();
+    }
+
+    @Test
+    void remove_RecursivelyDeleteDirectory_Success() throws AbstractApplicationException {
+        File subDir = new File(directory, "subDir");
+        File file1 = new File(directory, "file1.txt");
+        File file2 = new File(subDir, "file2.txt");
+
+        subDir.mkdir();
+        try {
+            file1.createNewFile();
+            file2.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // WHEN
+        rmApplication.remove(true, true, directory.getPath());
+
+        // THEN & VERIFY
+        assertFalse(directory.exists());
+        assertFalse(subDir.exists());
+        assertFalse(file1.exists());
+        assertFalse(file2.exists());
     }
 }
