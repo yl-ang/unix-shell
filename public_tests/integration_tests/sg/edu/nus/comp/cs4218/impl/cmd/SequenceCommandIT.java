003b645b5261fbf3e159a8125bc8d30821206f8e
Zheng Teck <adamgoh1998@gmail.com>
2024-03-25 01:11:44 +0800

    test: add integration test for sequence command

--- public_tests/integration_tests/sg/edu/nus/comp/cs4218/impl/cmd/SequenceCommandIT.java
+++ public_tests/integration_tests/sg/edu/nus/comp/cs4218/impl/cmd/SequenceCommandIT.java
@@ -1,9 +1,7 @@
 package integration_tests.sg.edu.nus.comp.cs4218.impl.cmd;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.io.TempDir;
 import sg.edu.nus.comp.cs4218.Command;
 import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
@@ -18,10 +16,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.CHAR_FILE_SEP;
-import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
+import static org.junit.jupiter.api.Assertions.*;
+import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.*;
 
 public class SequenceCommandIT {
     private SequenceCommand sequenceCommand;
@@ -31,39 +27,93 @@ public class SequenceCommandIT {
 
     private static final String STR_FILE_SEP = String.valueOf(CHAR_FILE_SEP);
     private static final String ROOT_DIRECTORY = Environment.currentDirectory;
-    private static final String[] TEST_DIRECTORY_ARR = {ROOT_DIRECTORY, "public_tests", "resources", "integration_tests", "sequence_command"};
-    private static final String TEST_DIRECTORY = String.join(STR_FILE_SEP, TEST_DIRECTORY_ARR);
     private static final String FOLDER_NAME_1 = "folder1";
     private static final String FOLDER_NAME_2 = "folder2"; // will be created and delete
     private static final String FOLDER_NAME_3 = "folder3"; // used for subfolder
-    private static final String FOLDER_NAME_SUB = FOLDER_NAME_3 + STR_FILE_SEP + "sub_folder";
+    private static final String FILE_NAME_OUTPUT = "tempfile.txt";
+    private static final String FOLDER_NAME_SUB = "sub_folder";
+    private static final String FOLDER_PATH_SUB = FOLDER_NAME_3 + STR_FILE_SEP + FOLDER_NAME_SUB;
     private static final String FOLDER_NAME_NON = "nonfolder";
     private static final String FILE_NAME_1 = "file1.txt";
     private static final String FILE_NAME_2 = "file2.txt";
-    private static final String FILE_NAME_NONE = "nonfile.txt";
+    private static final String FILE_NAME_3 = "file3.txt";
+    private static final String FILE_NAME_4 = "file4.txt";
+    private static final String FILE_NAME_5 = "file5.txt";
+    private static final String FILE_NAME_6 = "file6.txt";
+    private static final String FILE_GLOBBING = "*.txt";
 
+    @TempDir
+    File TEMP_DIRECTORY;
     @BeforeEach
-    void setup() {
-        Environment.currentDirectory = TEST_DIRECTORY;
+    void setup() throws IOException {
+        Environment.currentDirectory = TEMP_DIRECTORY.getAbsolutePath();
+
+        String folderPrefix = TEMP_DIRECTORY + STR_FILE_SEP;
+
+        FileWriter writer = new FileWriter(folderPrefix + FILE_NAME_1);
+        writer.write("bye world");
+        writer.close();
+
+        writer = new FileWriter(folderPrefix + FILE_NAME_2);
+        writer.write("""
+                1 but who?
+                2
+                9 but not me
+                4
+                5""");
+        writer.close();
+
+        writer = new FileWriter(folderPrefix + FILE_NAME_3);
+        writer.write("""
+                John
+                Woods
+                mAson
+                bowman
+                john
+                johhn
+                j0hn
+                johN
+                3 mason
+                q q
+                grep grep grep -h cs4218
+                """);
+        writer.close();
+
+        writer = new FileWriter(folderPrefix + FILE_NAME_4);
+        writer.write("""
+                apple
+                banana
+                banana
+                orange
+                orange
+                kiwi
+                """);
+        writer.close();
+
+        writer = new FileWriter(folderPrefix + FILE_NAME_5);
+        writer.write("""
+                banana
+                grape
+                apple
+                apple
+                orange""");
+        writer.close();
+
+        writer = new FileWriter(folderPrefix + FILE_NAME_6);
+        writer.write("""
+                Name,Age,Location
+                Kat,25,New York
+                Alice,30,Los Angeles
+                Bob,35,Chicago
+                """);
+        writer.close();
+
+        File folder1 = new File(folderPrefix + FOLDER_NAME_1);
+        // Folder 2 does not exist
+        assertTrue(folder1.mkdir());
+
         applicationRunner = new ApplicationRunner();
         outputStream = new ByteArrayOutputStream();
-    }
-
-    @AfterEach
-    void tearDown() throws IOException {
-        Path path = Path.of(TEST_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_2);
-        Path pathSub = Path.of(TEST_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_SUB);
-        Path pathParent = Path.of(TEST_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_3);
-
-        if (Files.exists(path)) {
-            Files.delete(path);
-        }
-        if (Files.exists(pathSub)) {
-            Files.delete(pathSub);
-        }
-        if (Files.exists(pathParent)) {
-            Files.delete(pathParent);
-        }
     }
 
     @AfterAll
@@ -71,7 +121,187 @@ public class SequenceCommandIT {
         Environment.currentDirectory = ROOT_DIRECTORY;
     }
 
-    // POSITIVE TEST CASE
+    // POSITIVE TEST CASE - PAIRWISE
+    @Test
+    @Tag("SequenceCommandIT:Pairwise:1")
+    public void callCommandIT_CallCommandThenCallCommand_ShouldReturnCorrectResult() throws ShellException, FileNotFoundException, AbstractApplicationException {
+        String commandInputStr = String.format("sort %s > %s; grep -iH john %s", FILE_NAME_3, FILE_NAME_OUTPUT, FILE_GLOBBING);
+        Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
+        sequenceCommand = (SequenceCommand) command;
+        sequenceCommand.evaluate(systemInputStream, outputStream);
+        String expectedOutput = """
+                file3.txt:John
+                file3.txt:john
+                file3.txt:johN
+                tempfile.txt:John
+                tempfile.txt:johN
+                tempfile.txt:john
+                """;
+        assertEquals(expectedOutput, outputStream.toString());
+        Path path = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FILE_NAME_OUTPUT);
+        assertTrue(Files.exists(path));
+    }
+
+    @Test
+    @Tag("SequenceCommandIT:Pairwise:2")
+    public void callCommandIT_CallCommandThenSequenceCommand_ShouldReturnCorrectResult() throws ShellException, FileNotFoundException, AbstractApplicationException {
+        String commandInputStr = String.format("wc %s%s%s; wc %s%s%s; wc %secho %s%s",
+                CHAR_DOUBLE_QUOTE, FILE_GLOBBING, CHAR_DOUBLE_QUOTE,
+                CHAR_SINGLE_QUOTE, FILE_GLOBBING, CHAR_SINGLE_QUOTE,
+                CHAR_BACK_QUOTE, FILE_GLOBBING, CHAR_BACK_QUOTE);
+        Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
+        sequenceCommand = (SequenceCommand) command;
+        sequenceCommand.evaluate(systemInputStream, outputStream);
+
+        String expectedOutput = """
+                wc: No such file or directory
+                wc: No such file or directory
+                \t0\t2\t9 file1.txt
+                \t4\t10\t29 file2.txt
+                \t11\t17\t82 file3.txt
+                \t6\t6\t39 file4.txt
+                \t4\t5\t31 file5.txt
+                \t4\t6\t70 file6.txt
+                \t29\t46\t260 total
+                """;
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    @Tag("SequenceCommandIT:Pairwise:3")
+    public void callCommandIT_CallCommandThenPipeCommand_ShouldReturnCorrectResult() throws ShellException, FileNotFoundException, AbstractApplicationException {
+        String inputText = "She sells sea shells by the sea shore.";
+        String commandInputStr = String.format("echo '%s' > %s; paste %s | grep sells", inputText, FILE_NAME_OUTPUT, FILE_NAME_OUTPUT);
+        Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
+        sequenceCommand = (SequenceCommand) command;
+        sequenceCommand.evaluate(systemInputStream, outputStream);
+
+        String expectedOutput = "She sells sea shells by the sea shore." + STRING_NEWLINE;
+        assertEquals(expectedOutput, outputStream.toString());
+
+        Path path = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FILE_NAME_OUTPUT);
+        assertTrue(Files.exists(path));
+    }
+
+    @Test
+    @Tag("SequenceCommandIT:Pairwise:4")
+    public void callCommandIT_SequenceCommandThenCallCommand_ShouldReturnCorrectResult() throws ShellException, FileNotFoundException, AbstractApplicationException {
+        String commandInputStr = String.format("echo Hello, World! > %s; cat %s ; rm %s", FILE_NAME_OUTPUT, FILE_NAME_OUTPUT, FILE_NAME_OUTPUT);
+        Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
+        sequenceCommand = (SequenceCommand) command;
+        sequenceCommand.evaluate(systemInputStream, outputStream);
+        String expectedOutput = "Hello, World!" + STRING_NEWLINE;
+        assertEquals(expectedOutput, outputStream.toString());
+        Path path = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FILE_NAME_OUTPUT);
+        assertFalse(Files.exists(path));
+    }
+
+    @Test
+    @Tag("SequenceCommandIT:Pairwise:5")
+    @Disabled
+    public void callCommandIT_SequenceCommandThenSequenceCommand_ShouldReturnCorrectResult() throws ShellException, FileNotFoundException, AbstractApplicationException {
+        String commandInputStr = String.format("mkdir -p %s; cd %s; ls; mv %s %s; rm -d %s", FOLDER_PATH_SUB, FOLDER_NAME_3, FOLDER_NAME_SUB, FOLDER_NAME_1, FOLDER_NAME_1);
+        Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
+        sequenceCommand = (SequenceCommand) command;
+        sequenceCommand.evaluate(systemInputStream, outputStream);
+        String expectedOutput = FOLDER_NAME_SUB + STRING_NEWLINE;
+        assertEquals(expectedOutput, outputStream.toString());
+        Path path = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_3);
+        assertTrue(Files.exists(path));
+        path = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_3 + STR_FILE_SEP + FOLDER_NAME_SUB);
+        assertFalse(Files.exists(path));
+        path = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_3 + STR_FILE_SEP + FOLDER_NAME_1);
+        assertFalse(Files.exists(path));
+    }
+
+    @Test
+    @Tag("SequenceCommandIT:Pairwise:6")
+    public void callCommandIT_SequenceCommandThenPipeCommand_ShouldReturnCorrectResult() throws ShellException, FileNotFoundException, AbstractApplicationException {
+        String commandInputStr = String.format("echo monkey; cat %s ; cat %s | sort -r | uniq", FILE_NAME_4, FILE_NAME_5);
+        Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
+        sequenceCommand = (SequenceCommand) command;
+        sequenceCommand.evaluate(systemInputStream, outputStream);
+        String expectedOutput = """
+                monkey
+                apple
+                banana
+                banana
+                orange
+                orange
+                kiwi
+                orange
+                grape
+                banana
+                apple
+                """;
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    @Tag("SequenceCommandIT:Pairwise:7")
+    @Disabled
+    public void callCommandIT_PipeCommandThenCallCommand_ShouldReturnCorrectResult() throws ShellException, FileNotFoundException, AbstractApplicationException {
+        String commandInputStr = String.format("echo Hello, World | tee %s %s /%s/%s; echo sequence_continued", FILE_NAME_1, FILE_NAME_2, FOLDER_NAME_NON, FILE_NAME_3);
+        Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
+        sequenceCommand = (SequenceCommand) command;
+        sequenceCommand.evaluate(systemInputStream, outputStream);
+
+        String expectedOutput = """
+                tee: /nonfolder/file3.txt: No such file or directory
+                Hello, World
+                sequence_continued
+                """;
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    @Tag("SequenceCommandIT:Pairwise:8")
+    public void callCommandIT_PipeCommandThenSequenceCommand_ShouldReturnCorrectResult() throws ShellException, FileNotFoundException, AbstractApplicationException {
+        String commandInputStr = "echo CS4218-CS4222-CS5222 | cut -b 2,6; echo total; echo 90";
+        Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
+        sequenceCommand = (SequenceCommand) command;
+        sequenceCommand.evaluate(systemInputStream, outputStream);
+
+        String expectedOutput = """
+                S8
+                total
+                90
+                """;
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    @Tag("SequenceCommandIT:Pairwise:9")
+    public void callCommandIT_PipeCommandThenPipeCommand_ShouldReturnCorrectResult() throws ShellException, IOException, AbstractApplicationException {
+        String commandInputStr = String.format("paste %s %s | sort -r > %s; paste %s %s | sort -f", FILE_NAME_6, FILE_NAME_5, FILE_NAME_OUTPUT, FILE_NAME_5, FILE_NAME_6 );
+        Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
+        sequenceCommand = (SequenceCommand) command;
+        sequenceCommand.evaluate(systemInputStream, outputStream);
+
+        String expectedOutput = """
+                apple\tAlice,30,Los Angeles
+                apple\tBob,35,Chicago
+                banana\tName,Age,Location
+                grape\tKat,25,New York
+                orange\t
+                """;
+        assertEquals(expectedOutput, outputStream.toString());
+
+        Path path = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FILE_NAME_OUTPUT);
+        assertTrue(Files.exists(path));
+
+        String fileContent = new String(Files.readAllBytes(path));
+        String expected = """
+                Name,Age,Location\tbanana
+                Kat,25,New York\tgrape
+                Bob,35,Chicago\tapple
+                Alice,30,Los Angeles\tapple
+                \torange
+                """;
+        assertEquals(expected, fileContent);
+    }
+
+    // POSITIVE TEST CASE - MILESTONE 1
     @Test
     public void sequenceCommandIT_EchoThenCd_ShouldReturnCorrectOutput() throws ShellException, FileNotFoundException, AbstractApplicationException {
         String commandInputStr = String.format("echo hello world; cd %s", FOLDER_NAME_1);
@@ -79,7 +309,7 @@ public class SequenceCommandIT {
         sequenceCommand = (SequenceCommand) command;
         sequenceCommand.evaluate(systemInputStream, outputStream);
         String expectedOutput = "hello world" + STRING_NEWLINE;
-        String expectedDir = TEST_DIRECTORY + CHAR_FILE_SEP + FOLDER_NAME_1;
+        String expectedDir = TEMP_DIRECTORY.getAbsolutePath() + CHAR_FILE_SEP + FOLDER_NAME_1;
         assertEquals(expectedOutput, outputStream.toString());
         assertEquals(expectedDir, Environment.currentDirectory);
     }
@@ -102,7 +332,7 @@ public class SequenceCommandIT {
         sequenceCommand.evaluate(systemInputStream, outputStream);
         String expectedOutput = "hello world" + STRING_NEWLINE;
         assertEquals(expectedOutput, outputStream.toString());
-        Path path = Path.of(TEST_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_2);
+        Path path = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_2);
         assertTrue(Files.exists(path));
     }
 
@@ -134,20 +364,20 @@ public class SequenceCommandIT {
         sequenceCommand.evaluate(systemInputStream, outputStream);
         String expectedOutput = "cd: No such file or directory" + STRING_NEWLINE + "\t10 " + FILE_NAME_2 + STRING_NEWLINE;
         assertEquals(expectedOutput, outputStream.toString());
-        assertEquals(TEST_DIRECTORY, Environment.currentDirectory);
+        assertEquals(TEMP_DIRECTORY.getAbsolutePath(), Environment.currentDirectory);
     }
 
     @Test
     public void sequenceCommandIT_CdThenMkdir_ShouldReturnCorrectOutput() throws ShellException, IOException, AbstractApplicationException {
-        String commandInputStr = String.format("cd %s; mkdir -p %s", FOLDER_NAME_NON, FOLDER_NAME_SUB);
+        String commandInputStr = String.format("cd %s; mkdir -p %s", FOLDER_NAME_NON, FOLDER_PATH_SUB);
         Command command = CommandBuilder.parseCommand(commandInputStr, applicationRunner);
         sequenceCommand = (SequenceCommand) command;
         sequenceCommand.evaluate(systemInputStream, outputStream);
         String expectedOutput = "cd: No such file or directory" + STRING_NEWLINE;
         assertEquals(expectedOutput, outputStream.toString());
 
-        Path path = Path.of(TEST_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_SUB);
-        Path pathParent = Path.of(TEST_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_3);
+        Path path = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FOLDER_PATH_SUB);
+        Path pathParent = Path.of(TEMP_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_3);
         assertTrue(Files.exists(path));
         assertTrue(Files.exists(pathParent));
     }
@@ -192,7 +422,7 @@ public class SequenceCommandIT {
         };
         String expectedOutput = String.join(STRING_NEWLINE, expectedOResultArr) + STRING_NEWLINE;
         assertEquals(expectedOutput, outputStream.toString());
-        String filePathStr = TEST_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_2;
+        String filePathStr = TEMP_DIRECTORY + STR_FILE_SEP + FOLDER_NAME_2;
         Path path = Path.of(filePathStr);
         assertTrue(Files.exists(path));
         assertEquals(Environment.currentDirectory, filePathStr);
